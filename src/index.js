a2202f1cd47f7aa22af68cff52fa78174ed31c52
Cory Boudreaux <cboud31@gmail.com>
2021-06-15 09:21:25 -0500

    Search component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {
   // Feature,
   Loading,
   // Preview,
-  // Search,
+  Search,
   Title,
 } from './components';
 
@@ -19,19 +19,26 @@ const App = () => {
    * featuredResult, setFeaturedResult (default should be null)
    * isLoading, setIsLoading (default should be false)
    */
+  const [searchResults, setSearchResults] = useState({
+    info: {},
+    records: [],
+  });
+  const [featuredResult, setFeaturedResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <div className="app">
       {/* <Title /> is static, doesn't need any props */}
       <Title />
       {/* <Search /> needs props for setIsLoading and setSearchResults (trigger <Loading /> on search start/end, and transfer results to preview) */}
-      {/* <Search /> */}
+      <Search setIsLoading={setIsLoading} setSearchResults={setSearchResults} />
       {/* <Preview /> needs props for searchResults, setIsLoading and setSearchResults (clicking prev/next buttons), and setFeaturedResult (clicking a preview) */}
       {/* <Preview /> */}
       {/* <Feature /> needs props for featuredResult, as well as setIsLoading and setSearchResults (clicking on searchable properties) */}
       {/* <Feature /> */}
       {/* <Loading /> is static, but should only render when isLoading is true */}
-      <Loading /> {/* use a ternary and render null if isLoading is false */}
+      {isLoading && <Loading />}
+      {/* use a ternary and render null if isLoading is false */}
     </div>
   );
 };
